35d64f10e458b3e638e164385b43f6561b30c64c
Filip Dutescu <filip.dutescu@gmail.com>
2019-09-12 22:59:05 +0300

    changed Source.cpp to have examples

--- SmallBetterLogger/Source.cpp
+++ SmallBetterLogger/Source.cpp
@@ -4,19 +4,19 @@ int main()
 {
 	std::string tmp = "Hello {0}";
 
+	// Declarations
 	sbl::Logger l;
-
-	l.WriteLine(tmp, "World");
-	l.Write("I am {0} and {1} years old.{2} {0}", "Filip", 20);
-	l.Write("{0}", "\n", "hey");
-
-	std::cerr << "Test\n\n";
-	std::clog << "Test\n";
-
 	sbl::logger logErr(sbl::STREAM_TYPE::STDERR);
-	logErr.WriteLine("logErr - {0}", "stderr");
 	sbl::logger logLog(sbl::STREAM_TYPE::STDLOG);
+
+	// Basic calls
+	l.WriteLine(tmp, "World");
+	l.Write("I am {0} and {1} years old.{2} {0}", "Michael", 28);
+	l.Write("{0}", "\n", "hey");
+	
+	logErr.WriteLine("logErr - {0}", "stderr");
 	logLog.WriteLine("logLog - {0}", "stdlog");
 
 	std::cin.get();
+	return 0;
 }
