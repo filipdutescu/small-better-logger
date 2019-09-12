cd36fe229db35d1e842216edb968f7f92fca3901
Filip Dutescu <filip.dutescu@gmail.com>
2019-09-12 23:19:57 +0300

    Added AutoFlush

--- SmallBetterLogger/Source.cpp
+++ SmallBetterLogger/Source.cpp
@@ -2,9 +2,21 @@
 
 int main()
 {
-	sbl::logger l("log.out");
+	std::string tmp = "Hello {0}";
 
-	l.Write("Test");
+	// Declarations
+	sbl::Logger l;
+	sbl::logger logErr(sbl::STREAM_TYPE::STDERR);
+	sbl::logger logLog(sbl::STREAM_TYPE::STDLOG);
+
+	// Basic calls
+	l.WriteLine(tmp, "World");
+	l.Write("I am {0} and {1} years old.{2} {0}", "Michael", 28);
+	l.Write("{0}", "\n", "hey");
+
+	logErr.WriteLine("logErr - {0}", "stderr");
+	logLog.WriteLine("logLog - {0}", "stdlog");
 
 	std::cin.get();
+	return 0;
 }
